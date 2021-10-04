93f8fd0031fdb2eca81d2f2f3de3c78d9542d994
Sheba Alikhan <shebaalikhan@yahoo.com>
2021-10-03 20:13:23 -0500

    removed line

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,6 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 MONTHS = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
 DAYS = ['all', 'monday', 'tuesday','wednesday','thursday','friday','saturday', 'sunday']
-
 def get_input(input_type, input_str):
     """
     Gets the input from user of the passed type.
